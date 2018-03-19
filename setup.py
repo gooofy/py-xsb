55d05fd17f481fd5be3d8d3782d4c7569f6f1c42
Guenter Bartsch <guenter@zamia.org>
2018-03-19 14:51:09 +0100

    0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name         ='py-xsb-prolog',
-      version      ='0.2.0',
+      version      ='0.2.1',
       description  ='Python interface for XSB Prolog',
       url          ='https://github.com/gooofy/py-xsb-prolog',
       classifiers  = [
