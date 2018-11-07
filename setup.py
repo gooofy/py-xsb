eb952094872f8af8ba2428897aa3b53c5340ee6b
Guenter Bartsch <guenter@zamia.org>
2018-11-07 11:27:41 +0100

    1.0.0 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name             ='py-xsb',
-      version          ='0.3.4',
+      version          ='1.0.0',
       description      ='Python interface for XSB',
       long_description = open('README.md').read(),
       url              ='https://github.com/gooofy/py-xsb',
